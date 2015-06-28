9e8ddd61c4e833997fa714fd03a1a886597c45b8
Kunal Nagar <knlnagar@gmail.com>
2015-06-28 18:25:01 +0530

    added console messages

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
-$(function() {
+$(function () {
 
-   $('.btn-download').click(function() {
-       var that = this;
-       $(this).attr('disabled', true);
-       $(this).find('i').removeClass('fa-download').addClass('fa-spinner fa-spin');
-       setTimeout(function() {
-           $(that).find('i').removeClass('fa-spinner fa-spin').addClass('fa-download');
-           $(that).attr('disabled', false);
-       }, 2000);
-       location.href = "https://downloads.wordpress.org/plugin/custom-404-pro.1.4.1.zip";
-   });
+    var faviconUrl = "http://kunalnagar.github.io/custom-404-pro/favicon.ico",
+        css = "background-image: url('" + faviconUrl + "');" +
+            "background-repeat: no-repeat;" +
+            "display: block;" +
+            "background-size: 13px 13px;" +
+            "padding-left: 13px;" +
+            "margin-left: 5px;",
+        text = "So. You like to investigate. I get it. You're a nosey dude. And I like fixing things. Visit www.kunalnagar.in/contact ";
+    if (navigator.userAgent.match(/chrome/i)) {
+        console.log(text + '%c', css);
+    } else {
+        console.log('%c   ' + text, css);
+    }
+
+    $('.btn-download').click(function () {
+        var that = this;
+        $(this).attr('disabled', true);
+        $(this).find('i').removeClass('fa-download').addClass('fa-spinner fa-spin');
+        setTimeout(function () {
+            $(that).find('i').removeClass('fa-spinner fa-spin').addClass('fa-download');
+            $(that).attr('disabled', false);
+        }, 2000);
+        location.href = "https://downloads.wordpress.org/plugin/custom-404-pro.1.4.1.zip";
+    });
 
 });
